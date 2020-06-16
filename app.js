8664e6c57a61f4b30c58262014f7772ed2024c25
pedro willian <pedrormx56@gmail.com>
2020-06-16 20:48:04 -0300

    Feira Virtual 1.0

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+let produtoTotal = 0
+let melancia = 4
+let banana = 5
+let uva = 2
+
+function add1melancia(){
+    document.getElementById('valorMelancia').innerHTML = `${produtoTotal+=1} Melancia`
+}
+function sub1melancia(){
+    document.getElementById('valorMelancia').innerHTML = `${produtoTotal-=1} Melancia`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let btn2 = document.getElementById('btn2')
 btn2.addEventListener('click', function(){
     window.location.href = 'register.html'
