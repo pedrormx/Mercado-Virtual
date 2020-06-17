bee7bb8a44ed77635894b1e91be2c61a1f8df8f1
pedro willian <pedrormx56@gmail.com>
2020-06-17 17:34:26 -0300

    Mercado Virtual 1.0

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ function sub1abacate(){
 
 }
 
+function exitIndex(){
+    window.location.href = 'login.html'
+}
 
 function mostraTotal(){
     let valorTotalMelancia = melancia * totalMelancia
     let valorTotalAbacate = abacate * totalAbacate
-    let valorTotal = valorTotalAbacate + valorTotalMelancia
-    document.getElementById('totalCompra').innerHTML = 1
-    
+     let valorTotal = valorTotalAbacate + valorTotalMelancia
+    console.log(valorTotal)    
+    document.getElementById('msg').innerHTML =`
+    Total: R$${valorTotal}.00 <br>
+    Melancia: R$${valorTotalMelancia}.00<br>
+    Abacate: R$${valorTotalAbacate}.00
+    `
+
+
 }
 
 let btn2 = document.getElementById('btn2')
 btn2.addEventListener('click', function(){
     window.location.href = 'register.html'
 })
-
