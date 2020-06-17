c939e466f8a62fd0d148fec428b364f3664a82cb
pedro willian <pedrormx56@gmail.com>
2020-06-17 13:50:32 -0300

    Mercado Virtual

--- app.js
+++ app.js
@@ -30,22 +30,6 @@ function mostraTotal(){
     ` 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let btn2 = document.getElementById('btn2')
 btn2.addEventListener('click', function(){
     window.location.href = 'register.html'
